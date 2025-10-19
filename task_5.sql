a564d517264b5f2a472d00480c0380a7d7cc5a20
Nkanyiso321 <nkanyisodube1234@gmail.com>
2025-10-19 18:23:45 +0200

    Add task_5.sql with customer table insertion

---
+++ task_5.sql
@@ -0,0 +1,4 @@
+-- Script to insert a single row into the customer table
+-- Database name passed as argument
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
