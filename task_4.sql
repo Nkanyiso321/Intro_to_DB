d48176350f3f210b57b7268dbf90aa5d91845e45
Nkanyiso321 <nkanyisodube1234@gmail.com>
2025-10-26 14:00:55 +0200

    Add script to show full description of books table (task_4.sql)

--- task_4.sql
+++ task_4.sql
@@ -1,17 +1,15 @@
 -- task_4.sql
--- Script to print the full description of the table 'books' from the database alx_book_store
+-- Print the full description of the table 'books' from alx_book_store
 
-SELECT 
-    COLUMN_NAME,
-    COLUMN_TYPE,
-    IS_NULLABLE,
-    COLUMN_KEY,
-    COLUMN_DEFAULT,
-    EXTRA
-FROM 
-    INFORMATION_SCHEMA.COLUMNS
-WHERE 
-    TABLE_SCHEMA = DATABASE()
-    AND TABLE_NAME = 'books'
-ORDER BY 
-    ORDINAL_POSITION;
+USE alx_book_store;
+
+SELECT COLUMN_NAME,
+       COLUMN_TYPE,
+       IS_NULLABLE,
+       COLUMN_KEY,
+       COLUMN_DEFAULT,
+       EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store'
+  AND TABLE_NAME = 'books';
+
