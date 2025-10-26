7bfe7ad35a7ee98e782da8dcfa7d26de04e5cec6
Nkanyiso321 <nkanyisodube1234@gmail.com>
2025-10-26 13:09:18 +0200

    Add task_6.sql: Insert multiple customers into alx_book_store database

--- task_4.sql
+++ task_4.sql
@@ -1,18 +1,2 @@
--- Script that prints the full description of the table books
--- from the database alx_book_store in the MySQL server.
--- The DESCRIBE or EXPLAIN statements are not allowed.
-
-SELECT
-    COLUMN_NAME AS Field,
-    COLUMN_TYPE AS Type,
-    IS_NULLABLE AS Null,
-    COLUMN_KEY AS Key,
-    COLUMN_DEFAULT AS Default,
-    EXTRA AS Extra
-FROM
-    INFORMATION_SCHEMA.COLUMNS
-WHERE
-    TABLE_SCHEMA = (SELECT DATABASE())
-    AND TABLE_NAME = 'books'
-ORDER BY
-    ORDINAL_POSITION;
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
