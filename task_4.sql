eaabcd7bb196f94d87647a8d6b9ae4169b930ea0
Nkanyiso321 <nkanyisodube1234@gmail.com>
2025-10-26 13:45:44 +0200

    Add task_4.sql: Completed SQL insert/update task

--- task_4.sql
+++ task_4.sql
@@ -1,15 +1,17 @@
 -- task_4.sql
--- Print the full description of the table books
--- from the database alx_book_store without using DESCRIBE or EXPLAIN
+-- Script to print the full description of the table 'books' from the database alx_book_store
 
 SELECT 
-    COLUMN_NAME AS 'Field',
-    COLUMN_TYPE AS 'Type',
-    IS_NULLABLE AS 'Null',
-    COLUMN_KEY AS 'Key',
-    COLUMN_DEFAULT AS 'Default',
-    EXTRA AS 'Extra'
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'alx_book_store'
-  AND TABLE_NAME = 'books';
-
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_KEY,
+    COLUMN_DEFAULT,
+    EXTRA
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = DATABASE()
+    AND TABLE_NAME = 'books'
+ORDER BY 
+    ORDINAL_POSITION;
