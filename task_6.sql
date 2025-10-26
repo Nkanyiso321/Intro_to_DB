8577d71fb12d194a11f1755b1fd6d7245da30271
Nkanyiso321 <nkanyisodube1234@gmail.com>
2025-10-26 12:56:12 +0200

    Add task_6.sql to insert multiple rows into customer table

--- task_6.sql
+++ task_6.sql
@@ -1,13 +1,5 @@
--- Inserts multiple rows into the 'customer' table of the 'alx_book_store' database.
--- The database name is assumed to be provided as an argument to the mysql command.
-
-INSERT INTO customer (
-    customer_id, 
-    customer_name, 
-    email, 
-    address
-)
-VALUES
+-- Insert multiple rows into the customer table in the alx_book_store database
+INSERT INTO customer (customer_id, customer_name, email, address) VALUES
 (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
 (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
 (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
